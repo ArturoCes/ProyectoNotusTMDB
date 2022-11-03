48a67177c15de1b8e23545eb6b7cf7ac0713ca97
titintoro <titintola@gmail.com>
2022-11-03 17:35:46 +0100

    Models

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true,
+  API_KEY: '184ee33b7d796a5c6f6d592d122d0947',
+  API_BASE: 'https://api.themoviedb.org/3'
 };
+
+// API KEY TITIN 184ee33b7d796a5c6f6d592d122d0947
